b313ebf8869df23e482a1cb1c3f1258951251e4e
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-09-08 16:23:17 -0700

    Update CustomActions.js

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -4,6 +4,7 @@ import { ref, uploadBytes } from "firebase/storage";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import * as Location from "expo-location";
+<<<<<<< Updated upstream
 
 let actionSheet;
 let pOnSend;
@@ -97,6 +98,110 @@ const onActionPress = () => {
                 default:
             }
         }
+=======
+import { storage } from "../firebase.config";
+import { getDownloadURL } from "firebase/storage";
+
+const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, userID }) => {
+    const actionSheet = useActionSheet();
+
+    const generateReference = (uri) => {
+        const timeStamp = new Date().getTime();
+        const imageName = uri.split("/")[uri.split("/").length - 1];
+        return `${userID}-${timeStamp}-${imageName}`;
+    };
+
+    const uploadAndSendImage = async (imageURI) => {
+        const uniqueRefString = generateReference(imageURI);
+        const newUploadRef = ref(storage, uniqueRefString);
+        const response = await fetch(imageURI);
+        const blob = await response.blob();
+        uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+            const imageURL = await getDownloadURL(snapshot.ref);
+            onSend({ image: imageURL });
+        });
+    };
+
+    const pickImage = async () => {
+        let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (permissions?.granted) {
+            let result = await ImagePicker.launchImageLibraryAsync();
+            if (!result.canceled) {
+                console.log("MADE IT");
+                try {
+                    await uploadAndSendImage(result.assets[0].uri);
+                } catch (error) {
+                    console.log(error);
+                }
+                return;
+            }
+            Alert.alert("Permissions haven't been granted.");
+        }
+    };
+
+    const takePhoto = async () => {
+        let permissions = await ImagePicker.requestCameraPermissionsAsync();
+        if (permissions?.granted) {
+            let result = await ImagePicker.launchCameraAsync();
+            if (!result.canceled) {
+                await uploadAndSendImage(result.assets[0].uri);
+                return;
+            }
+            Alert.alert("Permissions haven't been granted.");
+        }
+    };
+
+    const getLocation = async () => {
+        let permissions = await Location.requestForegroundPermissionsAsync();
+        if (permissions?.granted) {
+            const location = await Location.getCurrentPositionAsync({});
+            if (location) {
+                onSend({
+                    location: {
+                        longitude: location.coords.longitude,
+                        latitude: location.coords.latitude,
+                    },
+                });
+            } else {
+                Alert.alert("Error occurred while fetching location");
+            }
+        } else {
+            Alert.alert("Permissions haven't been granted.");
+        }
+    };
+
+    const onActionPress = () => {
+        const options = ["Choose From Library", "Take Picture", "Send Location", "Cancel"];
+        const cancelButtonIndex = options.length - 1;
+
+        actionSheet.showActionSheetWithOptions(
+            {
+                options,
+                cancelButtonIndex,
+            },
+            async (buttonIndex) => {
+                switch (buttonIndex) {
+                    case 0:
+                        pickImage();
+                        return;
+                    case 1:
+                        takePhoto();
+                        return;
+                    case 2:
+                        getLocation();
+                    default:
+                }
+            }
+        );
+    };
+
+    return (
+        <TouchableOpacity style={styles.container} onPress={onActionPress}>
+            <View style={[styles.wrapper, wrapperStyle]}>
+                <Text style={[styles.iconText, iconTextStyle]}>+</Text>
+            </View>
+        </TouchableOpacity>
+>>>>>>> Stashed changes
     );
 };
 
